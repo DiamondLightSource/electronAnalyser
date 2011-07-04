b53fc1acc84732c95865e1f2b097042894a1257b
James O'Hea <James.O'Hea@diamond.ac.uk>
2011-07-04 15:12:19 +0000

    Now set EPICS_CA_SERVER_PORT and EPICS_CA_REPEATER_PORT within startIOC.bat file to ensure can use caput/get, etc

--- startIOC.bat
+++ startIOC.bat
@@ -1,1 +1,9 @@
+
+echo Setting default EPICS ports...
+set  EPICS_CA_SERVER_PORT=6064
+echo EPICS_CA_SERVER_PORT=%EPICS_CA_SERVER_PORT%
+set  EPICS_CA_REPEATER_PORT=6065
+echo EPICS_CA_REPEATER_PORT=%EPICS_CA_REPEATER_PORT%
+
 C:\Projects\electronAnalyser\bin\win32-x86\electronAnalyser.exe C:\Projects\electronAnalyser\bin\win32-x86\stelectronAnalyser.boot
+
