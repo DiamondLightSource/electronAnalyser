9a5365f07dcb2cf19bcbc856ab0d235f4a3de009
James O'Hea <James.O'Hea@diamond.ac.uk>
2011-09-15 08:50:07 +0000

    Updated startIOC.bat to launch from C drive rather than W drive. Added macros to template file

--- startIOC.bat
+++ startIOC.bat
@@ -5,5 +5,6 @@ echo EPICS_CA_SERVER_PORT=%EPICS_CA_SERVER_PORT%
 set  EPICS_CA_REPEATER_PORT=6065
 echo EPICS_CA_REPEATER_PORT=%EPICS_CA_REPEATER_PORT%
 
-W:\work\R3.14.11\support\electronAnalyser\bin\win32-x86\electronAnalyser.exe W:\work\R3.14.11\support\electronAnalyser\bin\win32-x86\stelectronAnalyser.boot
+REM W:\work\R3.14.11\support\electronAnalyser\bin\win32-x86\electronAnalyser.exe W:\work\R3.14.11\support\electronAnalyser\bin\win32-x86\stelectronAnalyser.boot
+C:\Projects\electronAnalyser\bin\win32-x86\electronAnalyser.exe C:\Projects\electronAnalyser\bin\win32-x86\stelectronAnalyser.boot
 
