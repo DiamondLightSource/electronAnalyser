8f15f30a7d66e19255aeb53f6dc07ddf7f0cae64
James O'Hea <James.O'Hea@diamond.ac.uk>
2011-11-28 17:35:38 +0000

    Set EPICS_CA_MAX_ARRAY_BYTES in startIOC.bat file

--- startIOC.bat
+++ startIOC.bat
@@ -4,6 +4,8 @@ set  EPICS_CA_SERVER_PORT=6064
 echo EPICS_CA_SERVER_PORT=%EPICS_CA_SERVER_PORT%
 set  EPICS_CA_REPEATER_PORT=6065
 echo EPICS_CA_REPEATER_PORT=%EPICS_CA_REPEATER_PORT%
+set  EPICS_CA_MAX_ARRAY_BYTES=3000000
+echo EPICS_CA_MAX_ARRAY_BYTES=%EPICS_CA_MAX_ARRAY_BYTES%
 
 REM W:\work\R3.14.11\support\electronAnalyser\bin\win32-x86\electronAnalyser.exe W:\work\R3.14.11\support\electronAnalyser\bin\win32-x86\stelectronAnalyser.boot
 C:\Projects\electronAnalyser\bin\win32-x86\electronAnalyser.exe C:\Projects\electronAnalyser\bin\win32-x86\stelectronAnalyser.boot
