0e4320336238a8e74676e9da5b94e60d2812c501
Alan Greer <Alan.Greer@diamond.ac.uk>
2014-12-16 17:31:26 +0000

    Added builder support for the electronAnalyser network viewer areaDetector application.

--- etc/builder.py
+++ etc/builder.py
@@ -54,3 +54,36 @@ def electronAnalyser_sim(**kwargs):
     return simDetector(1024, 768, **kwargs)
 
 SetSimulation(electronAnalyser, electronAnalyser_sim)
+
+
+
+class _electronAnalyserViewer(AutoSubstitution):
+    TemplateFile="electronAnalyserViewer.template"
+    SubstitutionOverwrites = [_ADBaseTemplate]
+
+class electronAnalyserViewer(_ADBase):
+    '''Creates a electronAnalyserViewer driver'''
+    _SpecificTemplate = _electronAnalyserViewer
+    def __init__(self, BUFFERS = 50, MEMORY = -1, **args):
+        # Init the superclass
+        self.__super.__init__(**args)
+        # Store the args
+        self.__dict__.update(locals())
+        print self._SpecificTemplate
+
+    # __init__ arguments
+    ArgInfo = _ADBase.ArgInfo + _electronAnalyserViewer.ArgInfo + makeArgInfo(__init__,
+        BUFFERS = Simple('Maximum number of NDArray buffers to be created for '
+            'plugin callbacks', int),
+        MEMORY  = Simple('Max memory to allocate, should be maxw*maxh*nbuffer '
+            'for driver and all attached plugins', int))
+
+    LibFileList = ['electronAnalyserViewerSupport']
+    DbdFileList = ['electronAnalyserViewerSupport']
+
+    def Initialise(self):
+        print '# electronAnalyserViewerConfig(portName, maxBuffers, maxMemory)'
+        print 'electronAnalyserViewerConfig("%(PORT)s", %(BUFFERS)d, %(MEMORY)d)' % self.__dict__
+
+
+
